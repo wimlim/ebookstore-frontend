fad898e5cec8f342b8ea9ee06230a44153c308ba
wimlim <wimlim33@gmail.com>
2023-06-22 03:33:23 +0800

    fix ProfileView.js

--- src/components/AvatarUpload.js
+++ src/components/AvatarUpload.js
@@ -3,23 +3,41 @@ import { Typography, Avatar, Upload, message } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 const { Title } = Typography;
 
-const AvatarUpload = ({user, src, onChange }) => {
+const AvatarUpload = ({ user, src, onChange }) => {
     const [tempSrc, setTempSrc] = useState(null);
 
     const handleUpload = (info) => {
         if (info.file.status === 'uploading') {
-            // 在图片上传过程中显示临时预览图像
             setTempSrc(URL.createObjectURL(info.file.originFileObj));
         }
         if (info.file.status === 'done') {
-            // 使用上传的图片文件更新状态
             onChange(info.file.originFileObj);
-            setTempSrc(null); // 清除临时预览图像
-            message.success('upload successfully.');
+            setTempSrc(null);
+            message.success('上传成功');
         }
         if (info.file.status === 'error') {
-            //message.error('图片上传失败');
+            message.error('上传失败');
         }
+    };
+
+    const customRequest = ({ file, onSuccess, onError }) => {
+        const formData = new FormData();
+        formData.append('avatar', file);
+
+        fetch(`http://localhost:8080/users/avatar/${user}`, {
+            method: 'PUT',
+            body: formData,
+        })
+            .then((response) => {
+                if (response.ok) {
+                    onSuccess(null, response);
+                } else {
+                    throw new Error(response.statusText);
+                }
+            })
+            .catch((error) => {
+                onError(error);
+            });
     };
 
     return (
@@ -28,8 +46,7 @@ const AvatarUpload = ({user, src, onChange }) => {
             <Avatar shape="square" size={128} src={tempSrc || src} style={{ marginBottom: '24px' }} />
             <Upload
                 name="avatar"
-                action={`http://localhost:8080/users/avatar/${user}`}
-                method="PUT"
+                customRequest={customRequest}
                 onChange={handleUpload}
             >
                 <UploadOutlined /> 上传头像
