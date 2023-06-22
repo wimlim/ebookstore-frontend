dab7c8c5098975e9126b2d6ace6299bbadf0d7a9
wimlim <wimlim33@gmail.com>
2023-06-22 10:36:59 +0800

    fix ProfileView.js

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,9 +1,10 @@
-import axios from 'axios';
-
-async function fetchOrders() {
+export async function fetchOrders() {
     try {
-        const response = await axios.get('http://localhost:8080/orders/all');
-        const json = response.data;
+        const res = await fetch(`http://localhost:8080/orders/all`);
+        if (!res.ok) {
+            throw new Error(`${res.status} ${res.statusText}`);
+        }
+        const json = await res.json();
         const orders = json.map((timestampObj) => {
             const timestamp = timestampObj.timestamp;
             const items = timestampObj.items.map((item) => ({
@@ -21,9 +22,7 @@ async function fetchOrders() {
         });
         return orders;
     } catch (error) {
-        console.log(error);
-        throw error;
+        console.log("Error fetching data:", error);
+        throw new Error("Failed to fetch data. Please try again later.");
     }
 }
-
-export { fetchOrders };
