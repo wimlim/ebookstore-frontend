1238cd9d25c5a687ec92274bc4fac8534e2a7226
wimlim <wimlim33@gmail.com>
2024-01-06 06:03:57 +0800

    add neo4j

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
-import SideBar from "../routes/SideBar";
-import HeaderInfo from "../components/HeaderInfo";
 import SearchBar from "../components/SearchBar";
 import MyCarousel from "../components/Carousel";
 import BookList from "../components/BookList";
@@ -11,16 +8,34 @@ class HomeView extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            searchType: 'title',
             searchContent: '',
             books: []
         };
         this.handleSearch = this.handleSearch.bind(this);
+        this.handleSearchTypeChange = this.handleSearchTypeChange.bind(this);
     }
 
     async componentDidMount() {
         if (this.state.searchContent === '') {
             try {
                 const res = await fetch('http://localhost:8080/books');
+                const json = await res.json();
+                const books = json.map(data => ({
+                    id: data.id,
+                    name: data.title,
+                    price: data.price,
+                }));
+                this.setState({ books });
+            } catch (err) {
+                console.error('Error fetching data:', err);
+            }
+            return;
+        }
+
+        if (this.state.searchType == 'type') {
+            try {
+                const res = await fetch('http://localhost:8080/books/type/' + this.state.searchContent);
                 const json = await res.json();
                 const books = json.map(data => ({
                     id: data.id,
@@ -74,10 +89,25 @@ class HomeView extends Component {
             this.componentDidMount();
         });
     }
+    handleSearchTypeChange = (event) => {
+        this.setState({ searchType: event.target.value }, () => {
+            this.componentDidMount();
+        });
+    }
     render() {
         return (
             <div>
-                <SearchBar handleSearch={this.handleSearch} />
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '1rem' }}>
+                    <SearchBar handleSearch={this.handleSearch} />
+                    <select
+                        onChange={this.handleSearchTypeChange}
+                        value={this.state.searchType}
+                        style={{ marginLeft: '1rem' }}
+                    >
+                        <option value="title">按标题搜索</option>
+                        <option value="type">按类型搜索</option>
+                    </select>
+                </div>
                 <MyCarousel />
                 <BookList books={this.state.books} />
             </div>
